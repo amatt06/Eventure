15934c9cfaa32faa7e193e966ddcde31136815dc
amatt06 <s3853376@student.rmit.edu.au>
2024-09-16 18:35:50 +0100

    hash passwords

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,33 +1,46 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 // Define the schema
-const userSchema =
-    new mongoose.Schema({
-        firstName: {
-            type: String,
-            required: true
-        },
-        lastName: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true
-        },
-        password: {
-            type: String,
-            required: true
-        },
-        bio: {
-            type: String
-        },
-        accessLevel: {
-            type: Number,
-            required: true,
-        }
-    });
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    bio: {
+        type: String
+    },
+    accessLevel: {
+        type: Number,
+        required: true,
+    }
+});
+
+// Hash the password before saving
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
 
 // Create the User Model
 const User = mongoose.model('User', userSchema);
